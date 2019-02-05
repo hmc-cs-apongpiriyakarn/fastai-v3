67af04931f2148f58d79289abf8c0a7fd76a22a0
hmc-cs-apongpiriyakarn <35592070+hmc-cs-apongpiriyakarn@users.noreply.github.com>
2019-02-05 08:39:32 -0800

    change class names

--- app/server.py
+++ app/server.py
@@ -11,7 +11,7 @@ from fastai.vision import *
 export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1mxWRPPh-HzjgNn7rCQQfkRyJLcUhPka'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['persian', 'scottishfold', 'siamese']
 path = Path(__file__).parent
 
 app = Starlette()
